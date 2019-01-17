7f10b470d3f7a009ba5ba8cda7559402246c15f3
Josep <joseplajara7@gmail.com>
2019-01-17 09:50:44 +0100

    Subir a repo 17/01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularGrupo';
+export class AppComponent implements OnInit {
+  title = 'Pixart';
+
+  dimensiones = [333,888];
+
+
+
+   // Funcion que pide ancho y alto y lo devuelve
+
+    Tamanyo(){
+        //Pedir dimensiones
+        let ancho = parseInt(prompt("Introduzca los pixeles de ancho que desee"));
+        let alto = parseInt(prompt("Introduzca los pixeles de alto que desee"));
+
+        this.dimensiones = [ancho,alto];
+    }
+
+    ngOnInit(){
+        this.Tamanyo();
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TablaComponent } from './tabla/tabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaComponent
   ],
   imports: [
     BrowserModule
