5aa4e626676638169721e94a1ff33144dacdf1b1
Unknown <reginoboschlozano@gmail.com>
2019-01-21 12:53:08 +0100

    SUBIDA NOSE PARA QUE

--- src/app/tabla/tabla.component.ts
+++ src/app/tabla/tabla.component.ts
@@ -9,11 +9,36 @@ export class TablaComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
   @Input() dimensiones: number[];
 
+  columnas()
+  {
+    let columnas=[];
+    
+    for (let j = 0; j < 6; j++) {
+        columnas[j]="prueba";
+      
+    }
+    return columnas;
+  }
 
+  filas()
+  {
+    let filas=[];
+    for (let j = 0; j < 6; j++) {
+        filas[j]="prueba-fila";
+      
+    }
+    return filas;
+
+  }
+
+  ngOnInit()
+  {
+    let columnas: any[0] =this.columnas;
+    let filas: any[0] =this.columnas;
+
+  }
 
 }
